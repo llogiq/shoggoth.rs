16a80450c055114d1e074b9473846efbdfb8a163
Darin Morrison <darinmorrison+git@gmail.com>
2014-12-29 20:23:09 -0700

    Add docs

---
+++ doc/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['shoggoth'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='shoggoth/bit/struct._0.html' title='shoggoth::bit::_0'>_0</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='shoggoth/bit/struct._1.html' title='shoggoth::bit::_1'>_1</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='shoggoth/list/struct.Nil.html' title='shoggoth::list::Nil'>Nil</a>","<a class='stability Stable' title='Stable'></a>impl&lt;H: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>, T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> + <a class='trait' href='shoggoth/list/trait.List.html' title='shoggoth::list::List'>List</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='shoggoth/list/struct.Cons.html' title='shoggoth::list::Cons'>Cons</a>&lt;H, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.Sub.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['shoggoth'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;M: <a class='trait' href='shoggoth/nat/trait.IsNat.html' title='shoggoth::nat::IsNat'>IsNat</a>, N: <a class='trait' href='shoggoth/nat/trait.IsNat.html' title='shoggoth::nat::IsNat'>IsNat</a>, Rec: <a class='trait' href='shoggoth/nat/trait.IsNat.html' title='shoggoth::nat::IsNat'>IsNat</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Sub.html' title='core::ops::Sub'>Sub</a>&lt;<a class='struct' href='shoggoth/nat/struct.W.html' title='shoggoth::nat::W'>W</a>&lt;N&gt;&gt; for <a class='struct' href='shoggoth/nat/struct.W.html' title='shoggoth::nat::W'>W</a>&lt;M&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
