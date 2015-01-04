f6c9bd8165e2b2e28cf0892b3842edae0992a3ab
Darin Morrison <darinmorrison+git@gmail.com>
2015-01-04 02:27:13 -0700

    Change 'deriving' to 'derive'; fix warnings

--- src/hlist.rs
+++ src/hlist.rs
@@ -1,12 +1,12 @@
 pub trait HList {}
 
-#[deriving(Copy)]
-#[deriving(Show)]
+#[derive(Copy)]
+#[derive(Show)]
 pub struct HNil;
 impl HList for HNil {}
 
-#[deriving(Copy)]
-#[deriving(Show)]
+#[derive(Copy)]
+#[derive(Show)]
 pub struct HCons<H, T: HList>(pub H, pub T);
 impl<H, T: HList> HList for HCons<H, T> {}
 
