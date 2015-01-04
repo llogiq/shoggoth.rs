44f1ee4c7bab203201c33732bc0f8aa9299713b3
Darin Morrison <darinmorrison+git@gmail.com>
2015-01-04 02:49:33 -0700

    Derive standard traits for HNil and HCons

--- src/hlist.rs
+++ src/hlist.rs
@@ -1,11 +1,24 @@
 pub trait HList {}
 
+#[derive(Clone)]
 #[derive(Copy)]
+#[derive(Eq)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(PartialOrd)]
+#[derive(Rand)]
 #[derive(Show)]
 pub struct HNil;
 impl HList for HNil {}
 
+#[derive(Clone)]
 #[derive(Copy)]
+#[derive(Eq)]
+#[derive(Hash)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(PartialOrd)]
+#[derive(Rand)]
 #[derive(Show)]
 pub struct HCons<H, T: HList>(pub H, pub T);
 impl<H, T: HList> HList for HCons<H, T> {}
