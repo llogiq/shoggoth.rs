3186d794becc9c3c5442e1297430a6403a59e0de
Darin Morrison <darinmorrison+git@gmail.com>
2015-01-10 21:53:56 -0700

    LT should evaluate to True (not just be impl'd)

--- src/ty/fin.rs
+++ src/ty/fin.rs
@@ -1,3 +1,7 @@
+use ty::bool::{
+    False,
+    True,
+};
 use ty::fun;
 use ty::nat::{
     LT,
@@ -6,10 +10,10 @@ use ty::nat::{
     Z,
 };
 
-pub struct FZ<N: Nat> where LT: fun::Fn<(Z, N,)>;
-pub struct FS<N: Nat, P: Fin<N>> where LT: fun::Fn<(Z, N,)>;
+pub struct FZ<N: Nat> where LT: fun::Fn<(Z, N,), O = True>;
+pub struct FS<N: Nat, P: Fin<N>> where LT: fun::Fn<(Z, N,), O = True>;
 
 pub trait Fin<N: Nat> {}
-impl<N: Nat> Fin<N> for FZ<N> where LT: fun::Fn<(Z, N,)> {}
-impl<N: Nat, P: Fin<N>> Fin<S<N>> for FS<N, P> where LT: fun::Fn<(Z, N,)> {}
+impl<N: Nat> Fin<N> for FZ<N> where LT: fun::Fn<(Z, N,), O = True> {}
+impl<N: Nat, P: Fin<N>> Fin<S<N>> for FS<N, P> where LT: fun::Fn<(Z, N,), O = True> {}
 
