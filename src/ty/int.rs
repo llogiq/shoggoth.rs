3096e242c6f2133fabd3733b4f3d74f45d212d3c
Darin Morrison <darinmorrison+git@gmail.com>
2015-01-17 12:19:36 -0700

    Add terms for type ints in terms of pos bin nats

--- src/ty/int.rs
+++ src/ty/int.rs
@@ -1,5 +1,11 @@
 use ty::{
+    Tm,
     Ty,
+};
+use ty::nat::bin::pos as npos;
+use ty::bit::{
+    _0,
+    _1,
 };
 
 #[derive(Clone)]
@@ -12,3 +18,26 @@ use ty::{
 #[derive(Show)]
 pub enum Int {}
 impl Ty for Int {}
+
+#[derive(Clone)]
+#[derive(Copy)]
+#[derive(Eq)]
+#[derive(Hash)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(PartialOrd)]
+#[derive(Show)]
+pub enum Nz<P: Tm<npos::Pos>> {}
+#[derive(Clone)]
+#[derive(Copy)]
+#[derive(Eq)]
+#[derive(Hash)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(PartialOrd)]
+#[derive(Show)]
+pub enum Pz<P: Tm<npos::Pos>> {}
+
+impl Tm<Int> for _0 {}
+impl<N: Tm<npos::Pos>> Tm<Int> for Nz<N> {}
+impl<N: Tm<npos::Pos>> Tm<Int> for Pz<N> {}
