6b76325236dfee3e9a741459a7cd4f37c0a6a6ef
Darin Morrison <darinmorrison+git@gmail.com>
2015-01-31 00:13:33 -0700

    Modify exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,28 @@
 #![feature(rand)]
 #![feature(on_unimplemented)]
 
-pub use self::hlist::{
-    Cons,
-    HC,
-    HList,
-    HN,
-    IsCons,
-    Nil,
-};
-pub use self::product::{
-    ProductOps,
-    ToHList,
-    ToTuple,
-};
-pub use self::tuple::{
-    IsComposite,
-    TupleOps,
-};
+// pub use self::hlist::{
+//     Cons,
+//     HC,
+//     HList,
+//     HN,
+//     IsCons,
+//     Nil,
+// };
+// pub use self::product::{
+//     ProductOps,
+//     ToHList,
+//     ToTuple,
+// };
+// pub use self::tuple::{
+//     IsComposite,
+//     TupleOps,
+// };
 
-mod hlist;
-mod product;
-mod tuple;
 /// Heterogeneous lists
+pub mod hlist;
+// mod product;
+// mod tuple;
 
 /// Type-level programming
 pub mod ty;
