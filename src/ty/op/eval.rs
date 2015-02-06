82bef707449bd074805222c396e7118de78add84
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-06 14:53:48 -0700

    Formatting

--- src/ty/op/eval.rs
+++ src/ty/op/eval.rs
@@ -15,29 +15,33 @@ pub trait Eval<Fx>
     : HList
     + Tm<<<Fx as Infer>::Ty as IsArrow>::Dom>
 where
-          Fx: Infer<Mode = infer::mode::Constant>,
+      Fx: Infer<Mode = infer::mode::Constant>,
     <Fx as Infer>::Ty
-            : IsArrow,
+        : IsArrow,
 
 {
     type Out: Tm<<<Fx as Infer>::Ty as IsArrow>::Cod>;
 }
 
 pub trait Eval1<Fx> where
-              Fx: Infer,
-    HC<Self, HN>: Eval<Fx>,
-    HC<Self, HN>: Tm<<<Fx as Infer>::Ty as IsArrow>::Dom>,
-           <Fx as Infer>::Ty
-                : IsArrow,
+      Fx: Infer,
+    HC<Self, HN>
+        : Eval<Fx>,
+    HC<Self, HN>
+        : Tm<<<Fx as Infer>::Ty as IsArrow>::Dom>,
+    <Fx as Infer>::Ty
+        : IsArrow,
 {
     type Out: Tm<<<Fx as Infer>::Ty as IsArrow>::Cod>;
 }
 
 impl<Fx: Infer, Xs> Eval1<Fx> for Xs where
-      HC<Xs, HN>: Eval<Fx>,
-      HC<Xs, HN>: Tm<<<Fx as Infer>::Ty as IsArrow>::Dom>,
-           <Fx as Infer>::Ty
-                : IsArrow,
+    HC<Xs, HN>
+        : Eval<Fx>,
+    HC<Xs, HN>
+        : Tm<<<Fx as Infer>::Ty as IsArrow>::Dom>,
+    <Fx as Infer>::Ty
+        : IsArrow,
 {
     type Out = <HC<Xs, HN> as Eval<Fx>>::Out;
 }
