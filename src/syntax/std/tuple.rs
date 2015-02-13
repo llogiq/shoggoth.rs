1777eef300f4401c274940a01060f031c8909b04
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-12 21:31:09 -0700

    Add some stubs for tuple functionality

--- src/syntax/std/tuple.rs
+++ src/syntax/std/tuple.rs
@@ -10,13 +10,57 @@ pub trait TupleOps: Sized {
     }
 
     #[inline]
+    fn drop(self) {
+    }
+
+    #[inline]
     fn head(self) -> <Self as IsComposite>::H where Self: IsComposite {
         IsComposite::head(self)
     }
 
     #[inline]
+    fn init(self) {
+    }
+
+    #[inline]
+    fn last(self) {
+    }
+
+    #[inline]
+    fn length(self) {
+    }
+
+    #[inline]
+    fn map(self) {
+    }
+
+    #[inline]
+    fn flat_map(self) {
+    }
+
+    #[inline]
+    fn prepend(self) {
+    }
+
+    #[inline]
+    fn replace_at(self) {
+    }
+
+    #[inline]
+    fn reverse(self) {
+    }
+
+    #[inline]
+    fn split(self) {
+    }
+
+    #[inline]
     fn tail(self) -> <Self as IsComposite>::T where Self: IsComposite {
         IsComposite::tail(self)
+    }
+
+    #[inline]
+    fn take(self) {
     }
 }
 
