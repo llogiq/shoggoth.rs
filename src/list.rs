ea75d0ee2b6a5b40b55b1a673bffefb1b1359203
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-15 21:29:28 -0700

    Add nil/cons methods for List

--- src/list.rs
+++ src/list.rs
@@ -17,6 +17,16 @@ impl<A> ToSingleton for A {
 }
 
 #[rustc_on_unimplemented = "`{Self}` is not a heterogeneous list"]
-pub trait List {}
+pub trait List {
+    #[inline]
+    fn nil() -> Nil {
+        Nil
+    }
+
+    #[inline]
+    fn cons<X>(self, x: X) -> Cons<X, Self> where Self: Sized {
+        Cons(x, self)
+    }
+}
 impl List for Nil {}
 impl<H, T: List> List for Cons<H, T> {}
