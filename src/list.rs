ad95d5335e8d7d369e5d9d430c15d726eed85b84
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-15 21:28:15 -0700

    Add trait and blanket impl for list singleton op

--- src/list.rs
+++ src/list.rs
@@ -4,6 +4,18 @@ pub struct Nil;
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Cons<H, T: List>(pub H, pub T);
 
+pub trait ToSingleton {
+    type Out: List = Cons<Self, Nil>;
+
+    #[inline]
+    fn single(self) -> Cons<Self, Nil> where Self: Sized {
+        Cons(self, Nil)
+    }
+}
+impl<A> ToSingleton for A {
+    type Out = Cons<Self, Nil>;
+}
+
 #[rustc_on_unimplemented = "`{Self}` is not a heterogeneous list"]
 pub trait List {}
 impl List for Nil {}
