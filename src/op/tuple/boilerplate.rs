c7111fae248adc5fc105702f6ac845929b37a20e
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-15 22:19:25 -0700

    Clean up tuple boilerplate impls

--- src/op/tuple/boilerplate.rs
+++ src/op/tuple/boilerplate.rs
@@ -2,18 +2,21 @@ use syntax::product::{
     ToList,
     ToTuple,
 };
-use op::tuple;
+use op::tuple::{
+    IsPair,
+};
 
 macro_rules! impl_is_pair_for_seq {
     ($($seq:ident),*) => {
         #[allow(non_snake_case)]
-        impl<$($seq,)*> tuple::IsPair for ($($seq,)*) {
+        impl<$($seq,)*> IsPair for ($($seq,)*) {
             type H = seq_head!($($seq),*);
             type T = seq_tail!($($seq),*);
             #[inline]
             fn pair(self) -> (seq_head!($($seq),*), seq_tail!($($seq),*)) {
-                let ($($seq,)*) = self;
-                (seq_head!($($seq),*), seq_tail!($($seq),*))
+                match self {
+                    ($($seq,)*) => (seq_head!($($seq),*), seq_tail!($($seq),*))
+                }
             }
         }
     }
@@ -25,8 +28,10 @@ macro_rules! impl_to_list_for_seq {
         impl<$($seq,)*> Fn<(($($seq,)*),)> for ToList {
             type Output = List![$($seq),*];
             #[inline]
-            extern "rust-call" fn call(&self, (arg,): (($($seq,)*),)) -> List![$($seq),*] {
-                match arg { ($($seq,)*) => list![$($seq),*] }
+            extern "rust-call" fn call(&self, (this,): (($($seq,)*),)) -> List![$($seq),*] {
+                match this {
+                    ($($seq,)*) => list![$($seq),*]
+                }
             }
         }
     }
@@ -38,8 +43,8 @@ macro_rules! impl_to_tuple_for_seq {
         impl<$($seq,)*> Fn<(($($seq,)*),)> for ToTuple {
             type Output = ($($seq,)*);
             #[inline]
-            extern "rust-call" fn call(&self, (arg,): (($($seq,)*),)) -> ($($seq,)*) {
-                arg
+            extern "rust-call" fn call(&self, (this,): (($($seq,)*),)) -> ($($seq,)*) {
+                this
             }
         }
     }
