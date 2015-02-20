a1f13c080fb4e027a6d25ab40b43142f86dd3fa4
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-20 14:19:59 -0700

    Add binary naturals

--- src/nat.rs
+++ src/nat.rs
@@ -1,9 +1,24 @@
 pub use bit::*;
 
-mod pos {
+pub mod pos {
     use bit::*;
+
     pub trait Pos {}
     impl Pos for _1 {}
     impl<P: Pos, B> Pos for (P, B) {}
 }
 
+pub trait Nat {}
+impl Nat for _0 {}
+impl Nat for _1 {}
+impl<P: pos::Pos, B> Nat for (P, B) {}
+
+#[cfg(test)]
+mod test {
+    use bit;
+
+    #[test]
+    fn welp() {
+        let _: Nat!(42) = nat!(42);
+    }
+}
