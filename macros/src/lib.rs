22d78b206a389ed9070d6c9eaad26de8894b5471
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-20 03:39:31 -0700

    Remove unnecessary signature

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -30,8 +30,7 @@ use syntax::parse::token;
 }
 
 #[plugin_registrar]
-pub fn invoke_for_seq_upto_registrar(reg: &mut plugin::Registry) -> ()
-{
+pub fn invoke_for_seq_upto_registrar(reg: &mut plugin::Registry) {
     reg.register_macro("invoke_for_seq_upto", invoke_for_seq_upto_expand);
 }
 
