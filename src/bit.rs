588942f3e93f8963fe3bbd48bbc91ecf61afb3f6
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-20 17:30:16 -0700

    Start using PhantomData/PhantomFn

--- src/bit.rs
+++ src/bit.rs
@@ -1,9 +1,11 @@
+use std::marker::PhantomFn;
+
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct _0;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct _1;
 
-pub trait Bit {}
+pub trait Bit: PhantomFn<Self> {}
 impl Bit for _0 {}
 impl Bit for _1 {}
--- src/nat.rs
+++ src/nat.rs
@@ -1,14 +1,19 @@
-pub use bit::*;
+use bit::*;
+use std::marker::{
+    PhantomData,
+    PhantomFn,
+};
 
 pub mod pos {
     use bit::*;
+    use std::marker::PhantomFn;
 
-    pub trait Pos {}
+    pub trait Pos: PhantomFn<Self> {}
     impl Pos for _1 {}
     impl<P: Pos, B> Pos for (P, B) {}
 }
 
-pub trait Nat {}
+pub trait Nat: PhantomFn<Self> {}
 impl Nat for _0 {}
 impl Nat for _1 {}
 impl<P: pos::Pos, B> Nat for (P, B) {}
