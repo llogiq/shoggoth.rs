962f6d8bd7f79821c8ed12eb2b4e473162dcf459
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-23 21:11:43 -0700

    Add Reifies impl for PhantomData

--- src/reflect.rs
+++ src/reflect.rs
@@ -1,6 +1,19 @@
+use std::marker::{
+    PhantomData
+};
+
 pub trait Reifies {
     type Output;
     fn reflect(&self) -> Self::Output;
+}
+
+impl<A, Rec> Reifies for PhantomData<A> where
+    A: Reifies<Output = Rec>
+{
+    type Output = Rec;
+    fn reflect(&self) -> Rec {
+        unsafe { ::std::mem::transmute::<&PhantomData<A>, &A>(self).reflect() }
+    }
 }
 
 pub trait Reflects {
