d0ea09e0f77279a5e0d9f01a0e309f0d42794656
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-25 16:36:59 -0700

    Convert PredDouble impls to fam style

--- src/nat/boilerplate/pred_double.rs
+++ src/nat/boilerplate/pred_double.rs
@@ -10,28 +10,22 @@ use nat::ops::*;
 // Fn: PredDouble //////////////////////////////////////////////////////////////
 
 /// `pred_double(_1) ==> _1`
-impl Fn<(_1,)> for PredDouble {
-    type Output = _1;
-    #[inline]
-    extern "rust-call" fn call(&self, (_1 {},): (_1,)) -> _1 {
-        _1
+ty! { #[inline]
+    fam PredDouble(_1  ,) => _1 {
+        PredDouble(_1{},) => _1
     }
 }
 /// `pred_double(p:0) ==> pred_double(p):1`
-impl<P: Pos, Rec> Fn<((P, _0),)> for PredDouble where
-    PredDouble: Fn(P) -> Rec
-{
-    type Output = (Rec, _1);
-    #[inline]
-    extern "rust-call" fn call(&self, ((p, _0 {}),): ((P, _0),)) -> (Rec, _1) {
-        (PredDouble(p), _1)
-    }
+ty! { #[inline]
+    fam PredDouble((P, _0)  ,) => (Rec          , _1) {
+        PredDouble((p, _0{}),) => (PredDouble(p), _1)
+    } let {
+        Rec = PredDouble(P,),
+    } for .[ Rec ] :[ P: Pos ]
 }
 /// `pred_double(p:1) ==> p:0:1`
-impl<P: Pos> Fn<((P, _1),)> for PredDouble {
-    type Output = ((P, _0), _1);
-    #[inline]
-    extern "rust-call" fn call(&self, ((p, _1 {}),): ((P, _1),)) -> ((P, _0), _1) {
-        ((p, _0), _1)
-    }
+ty! { #[inline]
+    fam PredDouble((P, _1)  ,) => ((P, _0), _1) {
+        PredDouble((p, _1{}),) => ((p, _0), _1)
+    } for :[ P: Pos ]
 }
