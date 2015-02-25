59dc488298487b1b087f5663a7bb37b0218c54e4
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-25 16:43:49 -0700

    Convert Succ impls to fam style

--- src/nat/boilerplate/succ.rs
+++ src/nat/boilerplate/succ.rs
@@ -12,47 +12,37 @@ use nat::ops::*;
 // Fn: Succ ////////////////////////////////////////////////////////////////////
 
 // unwrap/rewrap
-impl<N: IsNat, Rec: IsNat> Fn<(W<N>,)> for Succ where
-    Succ: Fn(N) -> Rec
-{
-    type Output = W<Rec>;
-    #[inline]
-    extern "rust-call" fn call(&self, (W(n),): (W<N>,)) -> W<Rec> {
-        W(Succ.call((n,)))
-    }
+ty! { #[inline]
+    fam Succ(W<N>,) => W<Rec> {
+        Succ(W(m),) => W(Succ(m))
+    } let {
+        Rec = Succ(N,),
+    } for :[ N: IsNat, Rec: IsNat ]
 }
 
 /// `succ(0) ==> 1`
-impl Fn<(_0,)> for Succ {
-    type Output = _1;
-    #[inline]
-    extern "rust-call" fn call(&self, (_0 {},): (_0,)) -> _1 {
-        _1
+ty! { #[inline]
+    fam Succ(_0  ,) => _1 {
+        Succ(_0{},) => _1
     }
 }
 /// `succ(1) ==> 1:0`
-impl Fn<(_1,)> for Succ {
-    type Output = (_1, _0);
-    #[inline]
-    extern "rust-call" fn call(&self, (_1 {},): (_1,)) -> (_1, _0) {
-        (_1, _0)
+ty! { #[inline]
+    fam Succ(_1  ,) => (_1, _0) {
+        Succ(_1{},) => (_1, _0)
     }
 }
 /// `succ(p:0) ==> p:1`
-impl<LHS: Pos> Fn<((LHS, _0),)> for Succ {
-    type Output = (LHS, _1);
-    #[inline]
-    extern "rust-call" fn call(&self, ((lhs, _),): ((LHS, _0),)) -> (LHS, _1) {
-        (lhs, _1)
-    }
+ty! { #[inline]
+    fam Succ((P, _0  ),) => (P, _1) {
+        Succ((p, _0{}),) => (p, _1)
+    } for :[ P: Pos ]
 }
 /// `succ(p:1) ==> succ(p):0`
-impl<LHS: Pos, Rec> Fn<((LHS, _1),)> for Succ where
-    Succ: Fn(LHS) -> Rec
-{
-    type Output = (Rec, _0);
-    #[inline]
-    extern "rust-call" fn call(&self, ((lhs, _),): ((LHS, _1),)) -> (Rec, _0) {
-        (Succ(lhs), _0)
-    }
+ty! { #[inline]
+    fam Succ((P, _1  ),) => (Rec    , _0) {
+        Succ((p, _1{}),) => (Succ(p), _0)
+    } let {
+        Rec = Succ(P,),
+    } for .[ Rec ] :[ P: Pos ]
 }
