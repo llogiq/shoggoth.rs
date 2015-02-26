514802276803a466278c5c38c5f9badecc15a4f2
Darin Morrison <darinmorrison+git@gmail.com>
2015-02-26 10:42:18 -0700

    Rename boilerplate plugin module to products

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -10,13 +10,13 @@ extern crate rustc;
 
 use rustc::plugin;
 
-mod boilerplate;
 mod numerics;
+mod products;
 
 #[plugin_registrar]
 #[doc(hidden)]
 pub fn shoggoth_plugins_registrar(reg: &mut plugin::Registry) {
-    reg.register_macro("invoke_for_seq_upto", boilerplate::invoke_for_seq_upto_expand);
+    reg.register_macro("invoke_for_seq_upto", products::invoke_for_seq_upto_expand);
     reg.register_macro("Nat", numerics::nat_ty_expand);
     reg.register_macro("nat", numerics::nat_tm_expand);
 }
