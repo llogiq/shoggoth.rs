68fb5d4886451c9c1729889abe47709d6754a3b3
Darin Morrison <darinmorrison+git@gmail.com>
2015-03-02 19:13:57 -0700

    Support matching tail with list![=> x : xs]

--- src/macros/list.rs
+++ src/macros/list.rs
@@ -8,6 +8,19 @@
     { $lhs:ty, $($rest:ty),+ } => {
         $crate::list::Cons<$lhs, List![$($rest)+]>
     };
+    { $($lhs:ty),+ : $ret:ty } => {
+        ListRet!({ $ret } $($lhs),+)
+    };
+}
+macro_rules! ListRet {
+    { { $ret:ty } } => {
+        $ret
+    };
+    { { $ret:ty } $lhs:ty } => {
+        $crate::list::Cons<$lhs, $ret>
+    };
+    { { $ret:tt } $lhs:ty, $($rest:tt),+ } => {
+        $crate::list::Cons<$lhs, ListRet!({ $ret } $($rest),+)>
     };
 }
 
@@ -24,6 +37,20 @@
     { $head:expr, $($tail:expr),+ } => {
         $crate::list::Cons($head, list!($($tail),+))
     };
+    { $($lhs:expr),+ : $ret:expr } => {
+        list_expr_ret!({ $ret } $($lhs),+)
+    };
+}
+
+macro_rules! list_expr_ret {
+    { { $ret:expr } } => {
+        $ret
+    };
+    { { $ret:expr } $lhs:expr } => {
+        $crate::list::Cons($lhs, $ret)
+    };
+    { { $ret:tt } $lhs:expr, $($rest:tt),+ } => {
+        $crate::list::Cons($lhs, list_expr_ret!({ $ret } $($rest),+))
     };
 }
 
@@ -37,6 +64,19 @@ macro_rules! list_pat {
     { $lhs:pat, $($rest:tt),+ } => {
         $crate::list::Cons($lhs, list_pat!($($rest),+))
     };
+    { $($lhs:pat),+ : $ret:pat } => {
+        list_pat_ret!({ $ret } $($lhs),+)
     };
 }
 
+macro_rules! list_pat_ret {
+    { { $ret:pat } } => {
+        $ret
+    };
+    { { $ret:pat } $lhs:pat } => {
+        $crate::list::Cons($lhs, $ret)
+    };
+    { { $ret:tt } $lhs:pat, $($rest:tt),+ } => {
+        $crate::list::Cons($lhs, list_pat_ret!({ $ret } $($rest),+))
+    };
+}
