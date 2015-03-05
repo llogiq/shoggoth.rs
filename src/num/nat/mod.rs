87059c501b4fa990725a85714c6cffc113e3990d
Darin Morrison <darinmorrison+git@gmail.com>
2015-03-05 02:38:11 -0700

    Implement head operation

--- src/num/nat/mod.rs
+++ src/num/nat/mod.rs
@@ -1,2 +1,14 @@
-// use data::bit::*;
-// use data::nat::*;
+use data::bit::*;
+use data::nat::*;
+
+// Head ////////////////////////////////////////////////////////////////////////
+
+ty! {
+    fam Head :: Fn(Nat) -> Nat where
+        (O!(_1, Xs))        => _1        = { .. _1 } for { Xs }
+        (I!(_1, Xs))        => _1        = { .. _1 } for { Xs }
+        (O!(O!(X, Xs), Ys)) => O!(X, Xs) = { .. o!() } for { X, Xs, Ys }
+        (I!(O!(X, Xs), Ys)) => O!(X, Xs) = { .. o!() } for { X, Xs, Ys }
+        (O!(I!(X, Xs), Ys)) => I!(X, Xs) = { .. i!() } for { X, Xs, Ys }
+        (I!(I!(X, Xs), Ys)) => I!(X, Xs) = { .. i!() } for { X, Xs, Ys }
+}
